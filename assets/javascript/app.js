b4702dd936c9ec162ccea51bd4b6de2e04721ba6
mia-duv <56006479+mia-duv@users.noreply.github.com>
2020-04-16 14:03:35 -0400

    updating with jeopardy api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,87 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+var timer; 
 
-	function showQuestions(questions, quizContainer){
-		// code will go here
+var game = {
+
+correct: 0,
+incorrect: 0,
+counter: 10,
+
+countdown: function() {
+	game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
+    }
+},
+	
+ start: function() {
+
+	$("#btnNext").on("click", function(){
+	
+	timer = setInterval(game.countdown, 1000);
+	$("#sub-wrapper").prepend(
+		"<h2>Time Remaining: <span id='counter-number'>10</span> Seconds</h2>",
+		"<button id='btnReset'>Reset Game</button>"
+	  );
+   $("#btnNext").remove();
+	var queryURL = "http://jservice.io/api/random";
+$.ajax({
+	url: queryURL,
+	method: "GET"
+  })
+  .then(function(response) {
+	  console.log(response);
+	 currentAnswer = response[0].answer;
+	  $("#question").text("Question: " + response[0].question);
+	console.log(response[0].question);
+	console.log(response[0].answer);
+  
+  $("#submit").on("click", function() {
+	event.preventDefault();
+	clearInterval(timer);
+	$("#sub-wrapper h2").remove();
+	$("#answerDisplay").prepend(
+		"<h4>Answer<h4>",
+		"<p id='answer'></p>"
+	);
+	$("#answer").text("Answer: " + response[0].answer);
+	var userAnswer = $("#userAnswer").val().trim();
+
+    if(userAnswer.toLowerCase() === currentAnswer.toLowerCase()) {
+      console.log("Correct!");
 	}
-
-	function showResults(questions, quizContainer, resultsContainer){
-		// code will go here
+});
+})
+})
+},
+	done: function() {
+		
 	}
+};
+// $(function() {
+// 	$("#answerDisplay").hide();
+// }
+// )
+// $("#submit").on("click", function(){
+// 	$("#answerDisplay").show();
+// 	var queryURL = "http://jservice.io/api/random";
+// 	$.ajax({
+// 		url: queryURL,
+// 		method: "GET"
+// 	  })
+// 	  .then(function(response) {
+// 		  console.log(response);
+// 		 currentAnswer = response[0].answer;
+// 		  $("#answer").text("Answer: " + response[0].answer);
+	
+// 	  })
+// 	})
 
-	// show the questions
-	showQuestions(questions, quizContainer);
+	$(document).on("click", "#startGame", function() {
+		game.start();
+	  });
 
-	// when user clicks submit, show results
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
-	}
-}
+	$(document).on("click", "#btnReset", function() {
+		game.start();
+	});
