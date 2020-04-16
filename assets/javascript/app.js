4a8f9c0f405d80a89019572cebb4e04864632345
mia-duv <56006479+mia-duv@users.noreply.github.com>
2020-04-16 16:06:32 -0400

    reset button working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,6 @@
 var timer; 
-
 var game = {
 
-correct: 0,
-incorrect: 0,
 counter: 10,
 
 countdown: function() {
@@ -11,8 +8,13 @@ countdown: function() {
     $("#counter-number").html(game.counter);
     if (game.counter === 0) {
       console.log("TIME UP");
-      game.done();
-    }
+	  clearInterval(timer);
+	  $("#sub-wrapper h2").remove();
+	$("#answerDisplay").prepend(
+		"<p>'Time is up! Try again'</p>"
+	);
+	
+	}
 },
 	
  start: function() {
@@ -22,7 +24,8 @@ countdown: function() {
 	timer = setInterval(game.countdown, 1000);
 	$("#sub-wrapper").prepend(
 		"<h2>Time Remaining: <span id='counter-number'>10</span> Seconds</h2>",
-		"<button id='btnReset'>Reset Game</button>"
+		"<button id='btnReset'> Reset Game</button>",
+	
 	  );
    $("#btnNext").remove();
 	var queryURL = "http://jservice.io/api/random";
@@ -34,6 +37,9 @@ $.ajax({
 	  console.log(response);
 	 currentAnswer = response[0].answer;
 	  $("#question").text("Question: " + response[0].question);
+	  $("#userInput").prepend(
+		"<form> <input type='text' placeholder='Enter you answer here' id='userAnswer'><input type='button' value='Submit' id='submit'></form>"
+	  )
 	console.log(response[0].question);
 	console.log(response[0].answer);
   
@@ -46,42 +52,20 @@ $.ajax({
 		"<p id='answer'></p>"
 	);
 	$("#answer").text("Answer: " + response[0].answer);
-	var userAnswer = $("#userAnswer").val().trim();
 
-    if(userAnswer.toLowerCase() === currentAnswer.toLowerCase()) {
-      console.log("Correct!");
-	}
-});
 })
 })
-},
-	done: function() {
-		
+})
 	}
-};
-// $(function() {
-// 	$("#answerDisplay").hide();
-// }
-// )
-// $("#submit").on("click", function(){
-// 	$("#answerDisplay").show();
-// 	var queryURL = "http://jservice.io/api/random";
-// 	$.ajax({
-// 		url: queryURL,
-// 		method: "GET"
-// 	  })
-// 	  .then(function(response) {
-// 		  console.log(response);
-// 		 currentAnswer = response[0].answer;
-// 		  $("#answer").text("Answer: " + response[0].answer);
+
 	
-// 	  })
-// 	})
+};
+
 
 	$(document).on("click", "#startGame", function() {
 		game.start();
 	  });
 
-	$(document).on("click", "#btnReset", function() {
-		game.start();
-	});
+	  $(document).on("click", "#btnReset", function() {
+		window.location.reload(true);
+	  });
